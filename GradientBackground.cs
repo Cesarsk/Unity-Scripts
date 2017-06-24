16a5366c3005ad98694d54b01b066a8655751c40
Luca <luca.cesarano1@gmail.com>
2017-06-24 18:47:11 +0200

    Add files via upload

---
+++ GradientBackground.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+//this is a enhanced and updated version (works with Unity 5.6.1f1) of a script which allows users 
+//to set a gradient background in their camera component
+
+//this version of the script can change color dynamically (runtime) 
+//and loads a shader called Vertex Color Only.shader placed in assets/materials/
+
+public class GradientBackground : MonoBehaviour
+{
+    public Color topColor = Color.blue;
+    public Color bottomColor = Color.white;
+    public int gradientLayer = 7;
+    public static bool updateColorRuntime = false;
+    Mesh mesh;
+
+    private void Update()
+    {
+        if (!updateColorRuntime)
+        {
+            mesh.colors = new Color[4] { topColor, topColor, bottomColor, bottomColor };
+            //if you need to update just once, you can disable the variable after used it
+            updateColorRuntime = false;
+        }
+    }
+
+    void Awake()
+    {
+        gradientLayer = Mathf.Clamp(gradientLayer, 0, 31);
+        if (!GetComponent<Camera>())
+        {
+            Debug.LogError("Must attach GradientBackground script to the camera");
+            return;
+        }
+
+        GetComponent<Camera>().clearFlags = CameraClearFlags.Depth;
+        GetComponent<Camera>().cullingMask = GetComponent<Camera>().cullingMask & ~(1 << gradientLayer);
+        Camera gradientCam = new GameObject("Gradient Cam", typeof(Camera)).GetComponent<Camera>();
+        gradientCam.depth = GetComponent<Camera>().depth - 1;
+        gradientCam.cullingMask = 1 << gradientLayer;
+
+        mesh = new Mesh();
+        mesh.vertices = new Vector3[4]
+                        {new Vector3(-100f, .577f, 1f), new Vector3(100f, .577f, 1f), new Vector3(-100f, -.577f, 1f), new Vector3(100f, -.577f, 1f)};
+
+        mesh.colors = new Color[4] { topColor, topColor, bottomColor, bottomColor };
+
+        mesh.triangles = new int[6] { 0, 1, 2, 1, 3, 2 };
+        Shader shader = Shader.Find("Vertex Color Only");
+        Material mat = new Material(shader);
+        GameObject gradientPlane = new GameObject("Gradient Plane", typeof(MeshFilter), typeof(MeshRenderer));
+
+        ((MeshFilter)gradientPlane.GetComponent(typeof(MeshFilter))).mesh = mesh;
+        gradientPlane.GetComponent<Renderer>().material = mat;
+        gradientPlane.layer = gradientLayer;
+    }
+}
---
+++ MicrophoneInput.cs
@@ -0,0 +1,43 @@
+﻿//this snippet allows users to send input through microphone in Unity.
+//my suggestion is to set sensitivity with a slider so that this will work on many different smartphones.
+//this snippet is an updated and enhanced version of a script found somewhere with no copyrights.
+//the owner can contact me if he claims the property of this script
+
+bool flag = false;
+
+void MicrophoneInput()
+{
+    //get mic volume
+    int dec = 128;
+    float[] waveData = new float[dec];
+    int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
+    microphoneInput.GetData(waveData, micPosition);
+
+    // Getting a peak on the last 128 samples
+    float levelMax = 0;
+
+    for (int i = 0; i < dec; i++)
+    {
+        float wavePeak = waveData[i] * waveData[i];
+        if (levelMax < wavePeak) levelMax = wavePeak;
+    }
+
+    float level = Mathf.Sqrt(Mathf.Sqrt(levelMax));
+
+    //debug statements
+    if (debugMode)
+    {
+        //use some ui texts to tune the parameters
+        debugText.text = "level:" + level + "\nsensitivity: " + voiceSensitivity;
+        if (!flag) debugText2.text = "flag = False";
+        if (flag) debugText2.text = "flag = True";
+    }
+
+    if (level > voiceSensitivity && !flag)
+    {
+        doSomething();
+        flag = true;
+    }
+
+    if (level < voiceSensitivity && flag) flag = false;
+}
